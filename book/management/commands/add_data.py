48bd6b5ac0ddaaaf5de3fe543196c5959a0338d5
stevescilar <stevemuambi@gmail.com>
2022-12-22 16:19:15 +0300

    Update-pandas

--- book/management/commands/add_data.py
+++ book/management/commands/add_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from django.core.management.base  import BaseCommand
 from book.models import Book
+from sqlalchemy import create_engine
 
 class Command(BaseCommand):
     help = "A command to add data from an Excel file to the database"
@@ -9,4 +10,9 @@ class Command(BaseCommand):
 
         excel_file = "books.xlsx"
         df = pd.read_excel(excel_file)
-        print(df)
+        
+        engine = create_engine('sqlite:///db.sqlite3')
+
+
+        df.to_sql(Book._meta.db_table, if_exists='replace', con=engine, index=True)
+
